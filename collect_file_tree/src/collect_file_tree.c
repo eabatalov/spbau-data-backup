b6bb786c33177b5304dc62121cbcdd6ffd157d54
LizaTretyakova <elizabet.tretyakova@gmail.com>
2015-04-13 14:49:07 +0300

    This code gets segfault

--- collect_file_tree/src/collect_file_tree.c
+++ collect_file_tree/src/collect_file_tree.c
@@ -4,14 +4,17 @@
 
 int main(int argc, char* argv[]) {
 	struct file_tree tree;
-
+	struct dir_inode* tmp_to_get_tree_head;
+	
 	if(argc < 2) {
 		fprintf(stderr, "Error: not enough arguments\n");
 		exit(1);
 	}
 
-	tree.head = &((init_dir_inode(argv[1], NULL))->inode);
-	build_file_tree((struct dir_inode*)tree.head);
-	print_tree(tree.head, 0);
+	init_dir_inode(&tmp_to_get_tree_head, argv[1], NULL);
+	tree.head = (tmp_to_get_tree_head->inode);
+	build_file_tree((struct dir_inode*)&(tree.head));
+	print_tree(&(tree.head), 0);
+	//free_tree(&tree);
 	return 0;
 }
--- collect_file_tree/src/file_tree.c
+++ collect_file_tree/src/file_tree.c
@@ -8,18 +8,22 @@
 #include <string.h>
 
 void open_dir(char* name, DIR** dest) {
-	*dest = opendir(name);
+	fprintf(stderr, "%s\n", __func__);
+
+	(*dest) = opendir(name);
 	if(!(*dest)) {
 		perror("Error: couldn't open the directory");
 		exit(1);
 	}
 }
 
-struct regular_file_inode* init_reg_file_inode(struct dirent* source, struct inode* parent_dir) {
+void init_reg_file_inode(struct regular_file_inode** dest, struct dirent* source, struct inode* parent_dir) {
+	fprintf(stderr, "%s\n", __func__);
+
 	size_t len = strlen(source->d_name) + 1;//+1 for terminating null-byte
 	size_t parent_dir_len = 0;
-	struct regular_file_inode* dest = (struct regular_file_inode*)malloc(sizeof(struct regular_file_inode));
-	if(!dest) {
+	(*dest) = (struct regular_file_inode*)malloc(sizeof(struct regular_file_inode));
+	if(!(*dest)) {
 		perror("Error: failed to allocate memory");
 		exit(1);
 	}
@@ -27,37 +31,37 @@ struct regular_file_inode* init_reg_file_inode(struct dirent* source, struct ino
 		parent_dir_len = strlen(parent_dir->name) + 1;//+1 for '/' after the directory name
 	}
 	
-	dest->inode.type = INODE_REG_FILE;
+	(*dest)->inode.type = INODE_REG_FILE;
 	
-	dest->inode.name = (char*)malloc(len + parent_dir_len);
-	if(!dest->inode.name) {
+	(*dest)->inode.name = (char*)malloc(len + parent_dir_len);
+	if(!(*dest)->inode.name) {
 		perror("Error: unable to allocate memory");
 		exit(1);
 	}
 	if(parent_dir) {
-		strcpy(dest->inode.name, parent_dir->name);
-		strcat(dest->inode.name, "/");//maybe it's too platform-related
+		strcpy((*dest)->inode.name, parent_dir->name);
+		strcat((*dest)->inode.name, "/");//maybe too platform-related
 	}
 	else {
-		dest->inode.name[0] = '\0';
+		(*dest)->inode.name[0] = '\0';	
 	}
-	strcat(dest->inode.name, source->d_name);
+	strcat((*dest)->inode.name, source->d_name);
 		
-	dest->inode.parent = parent_dir;
-	if(stat(dest->inode.name, &dest->inode.attrs) < 0) {
+	(*dest)->inode.parent = parent_dir;
+	if(stat((*dest)->inode.name, &(*dest)->inode.attrs) < 0) {
 		perror("Error: failed to get regular file statistics");
 		exit(1);
 	}
-	dest->inode.user_data = NULL;
-	
-	return dest;
+	(*dest)->inode.user_data = NULL;
 }
 
-struct dir_inode* init_dir_inode(const char* dir_name, struct inode* parent_dir) {
+void init_dir_inode(struct dir_inode** dest, const char* dir_name, struct inode* parent_dir) {
+	fprintf(stderr, "%s\n", __func__);
+
 	size_t len = strlen(dir_name) + 1;
 	size_t parent_dir_len = 0;
-	struct dir_inode* dest = (struct dir_inode*)malloc(sizeof(struct dir_inode));
-	if(!dest) {
+	(*dest) = (struct dir_inode*)malloc(sizeof(struct dir_inode));
+	if(!(*dest)) {
 		perror("Error: unable to allocate memory");
 		exit(1);
 	}
@@ -65,35 +69,35 @@ struct dir_inode* init_dir_inode(const char* dir_name, struct inode* parent_dir)
 		parent_dir_len = strlen(parent_dir->name) + 1;
 	}
 	
-	dest->inode.type = INODE_DIR;
+	(*dest)->inode.type = INODE_DIR;
 	
-	dest->inode.name = (char*)malloc(len + parent_dir_len);
-	if(!dest->inode.name) {
+	(*dest)->inode.name = (char*)malloc(len + parent_dir_len);
+	if(!(*dest)->inode.name) {
 		perror("Error: unable to allocate memory");
 		exit(1);
 	}
 	if(parent_dir) {
-		strcpy(dest->inode.name, parent_dir->name);
-		strcat(dest->inode.name, "/");
+		strcpy((*dest)->inode.name, parent_dir->name);
+		strcat((*dest)->inode.name, "/");
 	}
 	else {
-		dest->inode.name[0] = '\0';
+		(*dest)->inode.name[0] = '\0';
 	}
-	strcat(dest->inode.name, dir_name);
+	strcat((*dest)->inode.name, dir_name);
 
-	dest->inode.parent = parent_dir;
-	if(stat(dest->inode.name, &dest->inode.attrs) < 0) {
+	(*dest)->inode.parent = parent_dir;
+	if(stat((*dest)->inode.name, &(*dest)->inode.attrs) < 0) {
 		perror("Error: failed to get regular file statistics");
 		exit(1);
 	}
-	dest->inode.user_data = NULL;
-	dest->num_children = 0;
-	dest->children = NULL;
-	
-	return dest;
+	(*dest)->inode.user_data = NULL;
+	(*dest)->num_children = 0;
+	(*dest)->children = NULL;
 }
 
 void walk_through_dir(DIR* dir, void (*f)(struct dirent*, void*), void* data) {
+	fprintf(stderr, "%s\n", __func__);
+
 	struct dirent* dir_content = readdir(dir);	
 	while(dir_content != NULL) {
 		f(dir_content, data);
@@ -101,30 +105,37 @@ void walk_through_dir(DIR* dir, void (*f)(struct dirent*, void*), void* data) {
 	}
 }
 
-void count(struct dirent* dir_content, void* data) {
+void implement_number_of_files_in_the_directory(struct dirent* dir_content, void* data) {
+	fprintf(stderr, "%s\n", __func__);
+
 	size_t* cnt = (size_t*)data;
 	++(*cnt);
 }
 
-size_t count_dir_content(DIR* dir) {
+size_t count_files_in_the_directory(DIR* dir) {
+	fprintf(stderr, "%s\n", __func__);
+
 	size_t res = 0;
-	walk_through_dir(dir, count, (void*)&res);
+	walk_through_dir(dir, implement_number_of_files_in_the_directory, (void*)&res);
 	return res;
 }
 
 void process_dir_child(struct dirent* dir_content, void* data) {
-	struct regular_file_inode* tmp_reg_file = (struct regular_file_inode*)malloc(sizeof(struct regular_file_inode));
-	struct dir_inode* tmp_dir = (struct dir_inode*)malloc(sizeof(struct dir_inode));
+	fprintf(stderr, "%s\n", __func__);
+
+	struct regular_file_inode* tmp_reg_file;
+	struct dir_inode* tmp_dir;
+	
 	struct dir_inode* parent = (struct dir_inode*)data;
 	
 	switch(dir_content->d_type) {
 		case DT_REG:
-			tmp_reg_file = init_reg_file_inode(dir_content, &(parent->inode));			
+			init_reg_file_inode(&tmp_reg_file, dir_content, &(parent->inode));			
 			parent->children[parent->num_children++] = &(tmp_reg_file->inode);
 			break;
 		case DT_DIR:
 			if(strcmp(dir_content->d_name, ".") != 0 && strcmp(dir_content->d_name, "..") != 0) {
-				tmp_dir = init_dir_inode(dir_content->d_name, &(parent->inode));
+				init_dir_inode(&tmp_dir, dir_content->d_name, &(parent->inode));
 				parent->children[parent->num_children++] = &(tmp_dir->inode);
 				build_file_tree(tmp_dir);
 			}
@@ -135,11 +146,15 @@ void process_dir_child(struct dirent* dir_content, void* data) {
 }
 
 void process_dir(DIR* dir, struct dir_inode* parent) {
+	fprintf(stderr, "%s\n", __func__);
+
 	walk_through_dir(dir, process_dir_child, (void*)parent);
 }
 
 void init_parent(DIR* dir, struct dir_inode* parent) {
-	size_t size = count_dir_content(dir);
+	fprintf(stderr, "%s\n", __func__);
+
+	size_t size = count_files_in_the_directory(dir);
 	rewinddir(dir);
 	
 	parent->children = (struct inode**)malloc(size * sizeof(struct inode*));
@@ -147,6 +162,8 @@ void init_parent(DIR* dir, struct dir_inode* parent) {
 }
 
 void build_file_tree(struct dir_inode* parent) {
+	fprintf(stderr, "%s\n", __func__);
+
 	DIR* current_dir;//is the directory which parent describes
 	
 	open_dir(parent->inode.name, &current_dir);
@@ -154,6 +171,8 @@ void build_file_tree(struct dir_inode* parent) {
 }
 
 void print_tree(struct inode* node, int space) {
+	fprintf(stderr, "%s\n", __func__);
+
 	int i, j;
 	for(i = 0; i < space; i++) {
 		printf("|	");
@@ -171,3 +190,42 @@ void print_tree(struct inode* node, int space) {
 		printf(ANSI_COLOR_RED "???\n" ANSI_COLOR_RESET);
 	}
 }
+
+void free_reg_file_inode(struct regular_file_inode* file_to_delete) {
+	fprintf(stderr, "%s\n", __func__);
+
+	fprintf(stderr, "file: %s %p\n", file_to_delete->inode.name, file_to_delete);
+	free(file_to_delete);
+}
+
+void free_dir_inode(struct dir_inode* dir_to_delete) {
+	fprintf(stderr, "%s\n", __func__);
+
+	int i = 0;
+	fprintf(stderr, "dir: %s %p\n", dir_to_delete->inode.name, dir_to_delete);
+	for(; i < dir_to_delete->num_children; i++) {
+		if(dir_to_delete->children[i]->type == INODE_DIR) {
+			free_dir_inode((struct dir_inode*)(dir_to_delete->children[i]));
+		}
+		else if(dir_to_delete->children[i]->type == INODE_REG_FILE){
+			free_reg_file_inode((struct regular_file_inode*)(dir_to_delete->children[i]));
+		}
+		else {
+			fprintf(stderr, "Something unidentified...\n");
+		}
+	}
+	free(dir_to_delete);
+}
+
+void free_tree(struct file_tree* tree) {
+	fprintf(stderr, "%s\n", __func__);
+
+	free_dir_inode((struct dir_inode*)&(tree->head));
+	fprintf(stderr, "freeing is completed\n");
+}
+
+/*
+todo:
+	1. to fix SegFault
+	2. to fix names
+*/
