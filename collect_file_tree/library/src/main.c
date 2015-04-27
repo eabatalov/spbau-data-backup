3206f662d1713a15914b10282df5fef4d23ff43e
LizaTretyakova <elizabet.tretyakova@gmail.com>
2015-04-27 16:25:56 +0300

    Improved src/main.c

--- collect_file_tree/library/src/main.c
+++ collect_file_tree/library/src/main.c
@@ -12,14 +12,14 @@ int bfs_visitor(struct inode* inode, void* data) {
 int dfs_visitor(struct inode* inode, void* data) {
 	int* r = (int*)data;
 	printf(ANSI_COLOR_GREEN "%s\n" ANSI_COLOR_RESET, inode->name);
-	//fprintf(stderr, "node: %s\n", inode->name);
+	fprintf(stderr, "node: %s\n", inode->name);
 	++(*r);
 	return (*r);
 }
 
 int main(int argc, char* argv[]) {
 	struct fs_tree* tree;
-	//int a = 0;
+	int a = 0;
 
 	if(argc < 2) {
 		fprintf(stderr, "Error: not enough arguments\n");
@@ -28,9 +28,9 @@ int main(int argc, char* argv[]) {
 
 	tree = fs_tree_collect(argv[1]);
 	fs_tree_print(tree);
-	//fs_tree_dfs(tree, dfs_visitor, &a);
-	//a = 0;
-	//fs_tree_bfs(tree, bfs_visitor, &a);
+	fs_tree_dfs(tree, dfs_visitor, &a);
+	a = 0;
+	fs_tree_bfs(tree, bfs_visitor, &a);
 	fs_tree_destroy(tree);
 	return 0;
 }
