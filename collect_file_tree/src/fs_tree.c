41d63737b3b2215e0e9886289ee987d893e1b443
LizaTretyakova <elizabet.tretyakova@gmail.com>
2015-05-09 15:11:42 +0300

    Oops.. removed some comments

--- collect_file_tree/src/fs_tree.c
+++ collect_file_tree/src/fs_tree.c
@@ -2,11 +2,7 @@
 #include <stdlib.h>
 #include <inodes.h>
 #include <fs_tree.h>
-/*
-#include <sys/types.h>
-#include <sys/stat.h>
-#include <unistd.h>
-*/
+
 
 struct fs_tree* fs_tree_collect(const char* path) {
 	struct fs_tree* tree = (struct fs_tree*)malloc(sizeof(struct fs_tree));
@@ -61,75 +57,3 @@ void fs_tree_destroy(struct fs_tree* tree) {
 void fs_tree_print(const struct fs_tree* tree) {
 	print_tree(tree->head, 0);
 }
-
-/*
-void dir_dfs(struct dir_inode* dir, fs_tree_inode_visitor visit, void* data) {
-	int i = 0;
-	int running = 1;
-	for(; i < dir->num_children && running; i++) {
-		running = visit(dir->children[i], data);
-		if(dir->children[i]->type == INODE_DIR) {
-			if(running) {
-				dir_dfs((struct dir_inode*)dir->children[i], visit, data);
-			}
-		}
-	}
-}
-
-void fs_tree_dfs(struct fs_tree* fs_tree, fs_tree_inode_visitor visit, void* data) {
-	if(fs_tree->head->type == INODE_DIR) {
-		dir_dfs((struct dir_inode*)fs_tree->head, visit, data);
-	}
-	else if(fs_tree->head->type == INODE_REG_FILE) {
-		visit(fs_tree->head, data);
-	}
-}
-*/
-
-/*
-int implement_number_of_files (struct inode* inode, void* data) {
-	size_t* cnt = (size_t*)data;
-	++(*cnt);
-	return 1;
-}
-
-size_t get_tree_size(struct fs_tree* fs_tree) {
-	size_t res = 0;
-	fs_tree_dfs(fs_tree, implement_number_of_files, &res);
-	return res;
-}
-
-void dir_bfs(fs_tree_inode_visitor visit, void* data, struct inode* queue[], 
-				int* queue_start, int* queue_end, int* running) {
-	
-	int i;
-	struct inode* node = queue[(*queue_start)++];
-	*running = visit(node, data);
-	if(node->type == INODE_DIR && *running) {
-		struct dir_inode* dir = (struct dir_inode*)node;
-		for(i = 0; i < dir->num_children; i++) {
-			queue[(*queue_end)++] = dir->children[i];
-		}
-	}
-}
-
-void fs_tree_bfs(struct fs_tree* fs_tree, fs_tree_inode_visitor visit, void* data) {
-	if(fs_tree->head->type == INODE_DIR) {
-		size_t queue_size = get_tree_size(fs_tree) + 1;
-		int queue_start = 0;
-		int queue_end = 0;
-		int running = 1;
-		struct inode** queue = (struct inode**)malloc(queue_size * sizeof(struct inode*));
-		
-		queue[queue_end++] = fs_tree->head;
-		while(queue_start < queue_end && running) {
-			dir_bfs(visit, data, queue, &queue_start, &queue_end, &running);
-		}
-	
-		free(queue);
-	}
-	else if (fs_tree->head->type == INODE_REG_FILE) {
-		visit(fs_tree->head, data);
-	}
-}
-*/
