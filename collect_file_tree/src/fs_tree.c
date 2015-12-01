a68bae6075d4144818a46b29ab6b20a453180052
Eugene Batalov <eabatalov89@gmail.com>
2015-12-01 17:27:37 +0300

    libcollect_file_tree: fix memory leak

--- collect_file_tree/src/fs_tree.c
+++ collect_file_tree/src/fs_tree.c
@@ -8,18 +8,14 @@ struct fs_tree* fs_tree_collect(const char* path) {
 	struct fs_tree* tree = (struct fs_tree*)malloc(sizeof(struct fs_tree));
 	struct regular_file_inode* reg_file_tmp;
 	struct dir_inode* dir_tmp; //tmp_to_get_tree_head = (struct dir_inode*)malloc(sizeof(struct dir_inode));
-	struct stat* buf = (struct stat*)malloc(sizeof(struct stat));
+	struct stat buf = {};
 
-	if(!buf) {
-		perror("Error: failed to allocate memory\n");
-		exit(1);
-	}
-	if(stat(path, buf) < 0) {
+	if(stat(path, &buf) < 0) {
 		perror("Error: failed to get file statistics\n");
 		exit(1);
 	}
 
-	if(S_ISDIR(buf->st_mode)) {
+	if(S_ISDIR(buf.st_mode)) {
 		dir_tmp = (struct dir_inode*)malloc(sizeof(struct dir_inode));
  		if(!dir_tmp) {
 			perror("Error: failed to allocate memory");
@@ -30,7 +26,7 @@ struct fs_tree* fs_tree_collect(const char* path) {
 		tree->head = &(dir_tmp->inode);
 		build_file_tree((struct dir_inode*)(tree->head));
 	}
-	else if(S_ISREG(buf->st_mode)) {
+	else if(S_ISREG(buf.st_mode)) {
 		reg_file_tmp = (struct regular_file_inode*)malloc(sizeof(struct regular_file_inode));
 		if(!reg_file_tmp) {
 			perror("Error: failed to allocate memory\n");
